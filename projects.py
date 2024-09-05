8b978e8a34272df85b8010eb826566e732739926
Ari Essunfeld <ariessunfeld@eworldes.com>
2024-09-05 10:55:18 -1000

    implemented support for local file loading and/or JIRA api loading, added projects.py for specifying APIs

---
+++ projects.py
@@ -0,0 +1,6 @@
+PROJECTS = {
+    "https://unisysbes.atlassian.net": "https://unisysbes.atlassian.net",
+    "Placeholder 1": "https://unisysbes.atlassian.net",
+    "Placeholder 2": "https://unisysbes.atlassian.net",
+    "Load from local file": "local"
+}
